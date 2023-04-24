1c8be373ca11cf19fe13e6338476e7725661d66d
Wilson <wilson.engenhariasoftware@gmail.com>
2023-04-24 16:45:20 -0300

    refactor: dados list

--- src/page/Home/dados.js
+++ src/page/Home/dados.js
@@ -2,43 +2,43 @@ export const list = [
     {
       id: 1,
       status: 'Realizado',
-      date: '08/04/2023',
+      date: '01/04/2023',
       type: 1 // 0 = Pendente - 1 = Realizado
     },
     {
       id: 2,
       status: 'Realizado',
-      date: '08/04/2023',
+      date: '02/04/2023',
       type: 1 // 0 = Pendente - 1 = Realizado
     },
     {
       id: 3,
       status: 'Pendente',
-      date: '08/04/2023',
+      date: '03/04/2023',
       type: 0 // 0 = Pendente - 1 = Realizado
     },
     {
       id: 4,
       status: 'Realizado',
-      date: '08/04/2023',
+      date: '04/04/2023',
       type: 1 // 0 = Pendente - 1 = Realizado
     },
     {
       id: 5,
       status: 'Realizado',
-      date: '08/04/2023',
+      date: '05/04/2023',
       type: 1 // 0 = Pendente - 1 = Realizado
     },
     {
       id: 6,
       status: 'Pendente',
-      date: '08/04/2023',
+      date: '06/04/2023',
       type: 0 // 0 = Pendente - 1 = Realizado
     },
     {
       id: 7,
       status: 'Realizado',
-      date: '08/04/2023',
+      date: '07/04/2023',
       type: 1 // 0 = Pendente - 1 = Realizado
     },
     {
@@ -50,25 +50,103 @@ export const list = [
     {
       id: 9,
       status: 'Pendente',
-      date: '08/04/2023',
+      date: '09/04/2023',
       type: 0 // 0 = Pendente - 1 = Realizado
     },
     {
       id: 10,
       status: 'Realizado',
-      date: '08/04/2023',
+      date: '10/04/2023',
       type: 1 // 0 = Pendente - 1 = Realizado
     },
     {
       id: 11,
       status: 'Realizado',
-      date: '08/04/2023',
+      date: '11/04/2023',
       type: 1 // 0 = Pendente - 1 = Realizado
     },
     {
       id: 12,
       status: 'Pendente',
-      date: '08/04/2023',
+      date: '12/04/2023',
+      type: 0 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 13,
+      status: 'Pendente',
+      date: '13/04/2023',
+      type: 1 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 14,
+      status: 'Pendente',
+      date: '14/04/2023',
+      type: 0 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 15,
+      status: 'Pendente',
+      date: '15/04/2023',
+      type: 1 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 16,
+      status: 'Pendente',
+      date: '16/04/2023',
+      type: 1 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 17,
+      status: 'Pendente',
+      date: '17/04/2023',
+      type: 1 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 18,
+      status: 'Pendente',
+      date: '18/04/2023',
+      type: 1 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 19,
+      status: 'Pendente',
+      date: '19/04/2023',
+      type: 1 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 20,
+      status: 'Pendente',
+      date: '20/04/2023',
+      type: 0 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 21,
+      status: 'Pendente',
+      date: '21/04/2023',
+      type: 1 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 22,
+      status: 'Pendente',
+      date: '22/04/2023',
+      type: 1 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 23,
+      status: 'Pendente',
+      date: '23/04/2023',
+      type: 1 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 24,
+      status: 'Pendente',
+      date: '24/04/2023',
+      type: 1 // 0 = Pendente - 1 = Realizado
+    },
+    {
+      id: 25,
+      status: 'Pendente',
+      date: '25/04/2023',
       type: 0 // 0 = Pendente - 1 = Realizado
     },
   ]
